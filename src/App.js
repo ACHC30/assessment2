267f6815e0dbbe8b95379c78f08cd6cfeb221252
icemaple77 <IceMaple7@gmail.com>
2022-04-27 23:41:38 +1000

    New NavBar

--- src/App.js
+++ src/App.js
@@ -1,28 +1,48 @@
-import "./App.css";
+import "./styles/App.css";
 import {
   BrowserRouter as Router,
   Routes,
   Route,
   NavLink,
 } from "react-router-dom";
-import Home from "./Routers/Home";
-import Stocks from "./Routers/Stocks";
-import PriceHistory from "./Routers/History";
+import {
+  Navbar,
+  Container,
+  Nav,
+  Form,
+  FormControl,
+  Button,
+} from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Home from "./Pages/Home";
+import Stocks from "./Pages/Stocks";
+import PriceHistory from "./Pages/History";
 
 function App() {
   return (
     <Router>
       <div>
-        <nav>
-          <ul>
-            <li>
-              <NavLink to="/">Home</NavLink>
-            </li>
-            <li>
-              <NavLink to="stocks">Stocks</NavLink>
-            </li>
-          </ul>
-        </nav>
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+          <Container>
+            <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+              <Nav className="me-auto">
+                <Nav.Link href="/">Home</Nav.Link>
+                <Nav.Link href="/stocks">Stocks</Nav.Link>
+              </Nav>
+              <Form className="d-flex">
+                <FormControl
+                  type="search"
+                  placeholder="Search"
+                  className="me-2"
+                  aria-label="Search"
+                />
+                <Button variant="outline-light">Search</Button>
+              </Form>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/stocks" element={<Stocks />} />
